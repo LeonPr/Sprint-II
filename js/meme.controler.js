13e584679307e8511080ce5a15b882fa39b89b7d
LeonPr <leon@masig.co.il>
2024-09-25 18:23:19 +0300

    Design canvas html

--- js/meme.controler.js
+++ js/meme.controler.js
@@ -1,37 +1,39 @@
 'use strict'
 
-function onInit(){
+function onInit() {
     appStart()
 }
 
-function renderImgs(filteredImgs=[]){
-    let strHTML=""
-    if(filteredImgs.length===0){
-        const imgsList= getImgs()
-        strHTML=imgsList.map(imgsList => `<img id="${imgsList.id}" src="${imgsList.url}" alt="" 
+function renderImgs(filteredImgs = []) {
+    let strHTML = ""
+    if (filteredImgs.length === 0) {
+        const imgsList = getImgs()
+        strHTML = imgsList.map(imgsList => `<img id="${imgsList.id}" src="${imgsList.url}" alt="" 
                                                onclick="onImgClick(this)">`)
-    }else{
-        strHTML=filteredImgs.map(imgsList => `<img id="${imgsList.id}" src="${imgsList.url}" alt="" 
+    } else {
+        strHTML = filteredImgs.map(imgsList => `<img id="${imgsList.id}" src="${imgsList.url}" alt="" 
             onclick="onImgClick(this)">`)
     }
     document.querySelector('.img-container').innerHTML = strHTML.join('')
 }
 
-function appStart(){
+function appStart() {
     renderImgs()
 }
 
-function onImgClick(elImg){
-    console.log('elImg', elImg);
+function onImgClick(elImg) {
+    setCurrentImgId(elImg.id)
+    window.location.href = "editMeme.html";
 }
-function onFilterInput(){
-    const imgsList= getImgs()
-    const elFilter = document.querySelector('input')
-    const filteredImgs = imgsList.filter( imgs => (imgs.keywords.includes(elFilter.value)))
 
-    if(filteredImgs.length !== 0){
+function onFilterInput() {
+    const imgsList = getImgs()
+    const elFilter = document.querySelector('input')
+    const filteredImgs = imgsList.filter(imgs => (imgs.keywords.includes(elFilter.value)))
+
+    if (filteredImgs.length !== 0) {
         renderImgs(filteredImgs)
-    }else{
+    } else {
         renderImgs()
     }
 
--- js/services/images.service.js
+++ js/services/images.service.js
@@ -1,60 +1,63 @@
 const IMG_KEY = 'ex-sprintII'
 
-var gImgGallery=[]
+var gImgGallery = []
+var gCurrentImgId = ''
 
 
-// var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
-_loadImages() 
+_loadImages()
 
 var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0, 
-    lines: [{ txt: 'I sometimes eat Falafel', size: 20, color: 'red' }] 
+  selectedImgId: 5,
+  selectedLineIdx: 0,
+  lines: [{ txt: 'I sometimes eat Falafel', size: 20, color: 'red' }]
 }
 
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
-function getImgs(){
+function getImgs() {
   return gImgGallery
 }
 
+function setCurrentImgId(imgId) {
+  gCurrentImgId = imgId
+}
+
 function _loadImages() {
-    gImgGallery = loadFromStorage(IMG_KEY)
+  gImgGallery = loadFromStorage(IMG_KEY)
 
-    if (gImgGallery && gImgGallery.length !== 0) return
-    console.log('string', createImg("/imgs/square/1.jpg",['human','politician','US','Donald Trump']));
-    gImgGallery=[]
+  if (gImgGallery && gImgGallery.length !== 0) return
+  gImgGallery = []
 
-    gImgGallery.push(createImg("imgs/square/1.jpg",['human','politician','US','Donald Trump']))
-    gImgGallery.push(createImg("imgs/square/2.jpg",['animals','dog','Labrador']))
-    gImgGallery.push(createImg("imgs/square/3.jpg",['animals','dog','human','baby']))
-    gImgGallery.push(createImg("imgs/square/4.jpg",['animals','cat']))
-    gImgGallery.push(createImg("imgs/square/5.jpg",['human','baby']))
-    gImgGallery.push(createImg("imgs/square/6.jpg",['human','television presenter','Giorgio A. Tsoukalos']))
-    gImgGallery.push(createImg("imgs/square/7.jpg",['human','baby']))
-    gImgGallery.push(createImg("imgs/square/8.jpg",['human','movie','actor','Gene Wilder','Willy Wonka']))
-    gImgGallery.push(createImg("imgs/square/9.jpg",['human','baby']))
-    gImgGallery.push(createImg("imgs/square/10.jpg",['human','politician','US','Barack Obama']))
-    gImgGallery.push(createImg("imgs/square/11.jpg",['human','SPORT','US','NBA','basketball','Ron Artest',' Paul Pierce ']))
-    gImgGallery.push(createImg("imgs/square/12.jpg",['human','television presenter','Israel','Haim Hecht']))
-    gImgGallery.push(createImg("imgs/square/13.jpg",['human','movie','actor','Leonardo DiCaprio','The Great Gatsby']))
-    gImgGallery.push(createImg("imgs/square/14.jpg",['human','movie','actor','Laurence Fishburne','Morpheus']))
-    gImgGallery.push(createImg("imgs/square/15.jpg",['human','movie','actor','Sean Bean','Boromir']))
-    gImgGallery.push(createImg("imgs/square/16.jpg",['human','movie','actor','Patrick Stewart','Jean-Luc Picard','Star Trek']))
-    gImgGallery.push(createImg("imgs/square/17.jpg",['human','politician','Russia','Vladimir Putin']))
-    gImgGallery.push(createImg("imgs/square/18.jpg",['animated','Woody','Buzz Lightyear']))
+  gImgGallery.push(createImg("imgs/square/1.jpg", ['human', 'politician', 'US', 'Donald Trump']))
+  gImgGallery.push(createImg("imgs/square/2.jpg", ['animals', 'dog', 'Labrador']))
+  gImgGallery.push(createImg("imgs/square/3.jpg", ['animals', 'dog', 'human', 'baby']))
+  gImgGallery.push(createImg("imgs/square/4.jpg", ['animals', 'cat']))
+  gImgGallery.push(createImg("imgs/square/5.jpg", ['human', 'baby']))
+  gImgGallery.push(createImg("imgs/square/6.jpg", ['human', 'television presenter', 'Giorgio A. Tsoukalos']))
+  gImgGallery.push(createImg("imgs/square/7.jpg", ['human', 'baby']))
+  gImgGallery.push(createImg("imgs/square/8.jpg", ['human', 'movie', 'actor', 'Gene Wilder', 'Willy Wonka']))
+  gImgGallery.push(createImg("imgs/square/9.jpg", ['human', 'baby']))
+  gImgGallery.push(createImg("imgs/square/10.jpg", ['human', 'politician', 'US', 'Barack Obama']))
+  gImgGallery.push(createImg("imgs/square/11.jpg", ['human', 'SPORT', 'US', 'NBA', 'basketball', 'Ron Artest', ' Paul Pierce ']))
+  gImgGallery.push(createImg("imgs/square/12.jpg", ['human', 'television presenter', 'Israel', 'Haim Hecht']))
+  gImgGallery.push(createImg("imgs/square/13.jpg", ['human', 'movie', 'actor', 'Leonardo DiCaprio', 'The Great Gatsby']))
+  gImgGallery.push(createImg("imgs/square/14.jpg", ['human', 'movie', 'actor', 'Laurence Fishburne', 'Morpheus']))
+  gImgGallery.push(createImg("imgs/square/15.jpg", ['human', 'movie', 'actor', 'Sean Bean', 'Boromir']))
+  gImgGallery.push(createImg("imgs/square/16.jpg", ['human', 'movie', 'actor', 'Patrick Stewart', 'Jean-Luc Picard', 'Star Trek']))
+  gImgGallery.push(createImg("imgs/square/17.jpg", ['human', 'politician', 'Russia', 'Vladimir Putin']))
+  gImgGallery.push(createImg("imgs/square/18.jpg", ['animated', 'Woody', 'Buzz Lightyear']))
 
-    _saveImgGallery()
+  _saveImgGallery()
+}
+
+function createImg(imgLink, keywords) {
+  return {
+    id: makeId(5),
+    url: `${imgLink}`,
+    keywords: keywords
   }
+}
 
-  function createImg(imgLink,keywords) {
-    return {
-        id: makeId(5),
-        url: `${imgLink}`,
-        keywords: keywords
-    }
-  }
-
-  function _saveImgGallery() {
-    saveToStorage(IMG_KEY, gImgGallery)
-  }
+function _saveImgGallery() {
+  saveToStorage(IMG_KEY, gImgGallery)
+}
